7da399dafcdd0bb25550a1b70e75a1baac348120
Harry tao <1047210553@qq.com>
2023-06-14 23:18:32 +0800

    update files

--- src/views/table/complex-table.vue
+++ src/views/table/complex-table.vue
@@ -4,25 +4,25 @@
       <el-card class="box-card">
         <div class="filter-container">
           <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-          <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px" class="filter-item">
+          <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px; margin-left: 20px;" class="filter-item">
             <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
           </el-select>
-          <el-select v-model="listQuery.type" placeholder="Type" clearable class="filter-item" style="width: 130px">
+          <el-select v-model="listQuery.type" placeholder="Type" clearable class="filter-item" style="width: 130px; margin-left: 20px;">
             <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />
           </el-select>
-          <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
+          <el-select v-model="listQuery.sort" style="width: 140px; margin-left: 20px;" class="filter-item" @change="handleFilter">
             <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
           </el-select>
-          <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+          <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" style="margin-left: 20px;" @click="handleFilter">
             Search
           </el-button>
-          <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
+          <el-button class="filter-item" style="margin-left: 20px;" type="primary" icon="el-icon-edit" @click="handleCreate">
             Add
           </el-button>
-          <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+          <el-button v-waves :loading="downloadLoading" class="filter-item" style="margin-left: 20px;" type="primary" icon="el-icon-download" @click="handleDownload">
             Export
           </el-button>
-          <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">
+          <el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:20px;" @change="tableKey=tableKey+1">
             reviewer
           </el-checkbox>
         </div>
